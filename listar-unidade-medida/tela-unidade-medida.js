6aaa51e66a47d33f25cf98635f7c6bac5f5527bc
Giullia Borges de Lima <GiulliaBorgesDe.Lima@philips.com>
2025-05-21 14:53:54 -0300

    correção da tela unidade de medida JS

--- listar-unidade-medida/tela-unidade-medida.js
+++ listar-unidade-medida/tela-unidade-medida.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const tabelaBody = document.getElementById('tabela-listagem-body');
 
+   
     
     const unidadeMedidaData = JSON.parse(localStorage.getItem("t09f-unidadeMedida")) || { values: [] };
 
@@ -29,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
        
         unidadeMedidaData.values.splice(index, 1);
 
-       
+        
         localStorage.setItem("t09f-unidadeMedida", JSON.stringify(unidadeMedidaData));
 
         
