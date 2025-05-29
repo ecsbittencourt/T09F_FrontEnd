e4ec6c36c9c007d1fec819f35ac514a5de60c246
Bernardo Gums Brack <bernardogums.brack@philips.com>
2025-05-29 10:54:23 -0300

    fix tela cadastrar setor

--- cadastrar-setor/tela-cadastrar-setor.js
+++ cadastrar-setor/tela-cadastrar-setor.js
@@ -1,7 +1,7 @@
-const cadastrarUnidade = async (event) => {
+const cadastrarSetor = async (event) => {
   event.preventDefault();
 
-  const medidaValor = document.getElementById("input-unidade").value.trim();
+  const medidaValor = document.getElementById("input-setor").value.trim();
 
   if (!medidaValor) {
     alert("Por favor, informe a unidade de medida.");
@@ -9,29 +9,29 @@ const cadastrarUnidade = async (event) => {
   }
 
   try {
-    const response = await fetch("http://127.0.0.1:8080/api/unidades-medida", {
+    const response = await fetch("http://127.0.0.1:8080/api/setores", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ medida: medidaValor })
+      body: JSON.stringify({ nome: medidaValor })
     });
 
     if (!response.ok) {
-      alert("Erro ao criar unidade de medida.");
+      alert("Erro ao criar setor.");
       return;
     }
 
-    alert("Unidade de medida criada com sucesso!");
-    document.getElementById("input-unidade").value = "";
+    alert("Setor criada com sucesso!");
+    document.getElementById("input-setor").value = "";
 
   } catch (error) {
     alert("Erro desconhecido");
   }
 };
 
-document.getElementById("botao-criar-unidade").addEventListener("click", cadastrarUnidade);
+document.getElementById("botao-criar-setor").addEventListener("click", cadastrarSetor);
 
-document.getElementById("input-unidade").addEventListener("keydown", (e) => {
+document.getElementById("input-setor").addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    cadastrarUnidade(e);
+    cadastrarSetor(e);
   }
 });
